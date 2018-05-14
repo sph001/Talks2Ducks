7e059e2b829ec0e077fac680ff521626b603f00c
Steven Hirschmann <StevenHirschmann14@gmail.com>
2018-05-14 21:52:15 +0100

    --init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+          <h1>What is this?</h1>
+          <div>Follow along in real time as I progress through developing a blog and portfolio site from scratch!</div>
+          <div>I will be building this site incrementally while simultaneously developing a series of tutorials.</div>
+      </div>
+    );
+  }
+}
+
+export default App;
